83b1b521c0faa5d0c4aa5c446fd33f837a9fad77
drakonoff <drakonoff@inbox.ru>
2025-11-30 00:26:10 +0300

    Add production server IP to ALLOWED_HOSTS

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hvxn%qq=gyw^4*o2lo1#b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '0') == '1'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,0.0.0.0,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,0.0.0.0,localhost,185.177.219.84').split(',')
 
 INTERNAL_IPS = [
     "127.0.0.1",
